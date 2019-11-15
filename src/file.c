35e64dd9e1168de0a2f2ec2c1f14935cfaa622d9
Caryl Ellis <caellis@mi-u5.21-school.ru>
2019-11-15 12:12:30 +0300

    Revert "norminette workinprogress"
    This reverts commit 2818e3e5345f0bbd35bcef4b83d2b3a5cc35a9e9.

--- src/file.c
+++ src/file.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   file.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ch3rryhq <ch3rryhq@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 19:12:57 by bnesoi            #+#    #+#             */
-/*   Updated: 2019/11/14 17:12:36 by caellis          ###   ########.fr       */
+/*   Updated: 2019/11/13 19:18:37 by ch3rryhq         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
 int		new_file(const char dir_name[PATH_MAX],
-			const struct dirent *ent, t_file **file)
+	const struct dirent *ent, t_file **file)
 {
 	if (!(*file = (t_file *)ft_memalloc(sizeof(t_file))))
 		return (0);
@@ -43,7 +43,7 @@ void	file_to_tab(char tab[][NAME_MAX + 1], t_file *f)
 }
 
 void	file_iter(t_file *file, t_list_layout *layout, unsigned int flags,
-			void (*f)(t_file *file, t_list_layout *layout, unsigned int flags))
+				void (*f)(t_file *file, t_list_layout *layout, unsigned int flags))
 {
 	while (file && f)
 	{
@@ -63,5 +63,5 @@ void	file_free(t_file **file)
 		free(tmp);
 	}
 	*file = NULL;
-	return ;
+    return ;
 }
