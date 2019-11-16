a2778038d3754d84f573afa392f123c3b3ca9f8d
Caryl Ellis <caellis@mi-u3.21-school.ru>
2019-11-16 13:22:19 +0300

    NORME DONE

--- src/put.c
+++ src/put.c
@@ -6,13 +6,13 @@
 /*   By: caellis <caellis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/20 19:01:51 by bnesoi            #+#    #+#             */
-/*   Updated: 2019/11/15 16:01:05 by caellis          ###   ########.fr       */
+/*   Updated: 2019/11/16 13:15:48 by caellis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	put_list_file(t_file *f, t_list_layout *l, unsigned int flags)
+static void	put_list_file(t_file *f, t_list_layout *l, unsigned int flags)
 {
 	char			str_chmod[12];
 	char			str_time[12];
@@ -40,44 +40,47 @@ void	put_list_file(t_file *f, t_list_layout *l, unsigned int flags)
 			S_ISLNK(f->stat.st_mode) ? link : "");
 }
 
-void	put_file(t_file *f, t_list_layout *l, unsigned int flags)
+static void	tab_printer(char tab[][NAME_MAX + 1], char *out, t_list_layout *l)
 {
-	char			files[l->n_files + 1][NAME_MAX + 1];
-	char			out[l->n_files * (l->maxlen + 1) + 1];
-	char			*tmp;
+	unsigned short	offset;
 	unsigned short	i;
 	unsigned short	pos;
-	unsigned short	offset;
 
-	ft_bzero((void *)out, sizeof(l->n_files * (l->maxlen + 1) + 1));
 	i = 0;
-	tmp = out;
-	(void)flags;
-	file_to_tab(files, f);
 	offset = 0;
 	while (i < l->rows)
 	{
-		pos = i;
+		pos = i++;
 		while (pos < l->n_files)
 		{
-			offset += ft_sprintf(tmp + offset, "%-*s ",
-				pos + l->rows < l->n_files ? l->maxlen : 1, files[pos]);
+			offset += ft_sprintf(out + offset, "%-*s ",
+				pos + l->rows < l->n_files ? l->maxlen : 1, tab[pos]);
 			pos += l->rows;
 		}
-		ft_strcpy(tmp + offset, "\n");
-		offset++;
-		i++;
+		ft_strcpy(out + offset++, "\n");
 	}
 	ft_putstr(out);
 }
 
-void	put_file_recursive(t_file *file, unsigned int flags)
+static void	put_file(t_file *f, t_list_layout *l, unsigned int flags)
+{
+	char			files[l->n_files + 1][NAME_MAX + 1];
+	char			out[l->n_files * (l->maxlen + 1) + 1];
+
+	ft_bzero((void *)out, sizeof(l->n_files * (l->maxlen + 1) + 1));
+	file_to_tab(files, f);
+	tab_printer(files, out, l);
+	(void)flags;
+}
+
+void		put_file_recursive(t_file *file, unsigned int flags)
 {
 	t_file	*tmp;
 
 	while (file)
 	{
-		if (S_ISDIR(file->stat.st_mode) && !IS_CD(file->name) && !IS_UP(file->name))
+		if (S_ISDIR(file->stat.st_mode) &&
+			!IS_CD(file->name) && !IS_UP(file->name))
 		{
 			ft_putstr("\n");
 			list_dir(file->file_name, flags);
@@ -88,7 +91,7 @@ void	put_file_recursive(t_file *file, unsigned int flags)
 	}
 }
 
-void	put_file_switch(t_file *file, t_list_layout *l, unsigned int flags)
+void		put_file_switch(t_file *file, t_list_layout *l, unsigned int flags)
 {
 	if (flags & LSF_L)
 		file_iter(file, l, flags, put_list_file);
@@ -102,7 +105,8 @@ void	put_file_switch(t_file *file, t_list_layout *l, unsigned int flags)
 	}
 	else
 	{
-		l->cols = l->w.ws_col / (l->maxlen + 1) <= 1 ? 1 : l->w.ws_col / (l->maxlen + 1) - 1;
+		l->cols = l->w.ws_col / (l->maxlen + 1) <= 1 ?
+			1 : l->w.ws_col / (l->maxlen + 1) - 1;
 		l->rows = l->n_files / l->cols ? l->n_files / l->cols : 1;
 		put_file(file, l, flags);
 	}
